2dcef6b1bb19967847ad74acd434a54b80e9672d
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-19 19:45:13 +0530

    Resolve conflict

--- Hello_world.py
+++ Hello_world.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print( "Hello world!")
 
  # 1 Basic print
@@ -17,23 +16,3 @@ print(message)
 name = "World"
 print(f"Hello, {name}!")
 
-=======
-print( "Hello world!")
-
- # 1 Basic print
-print("Hello, World!")
-
-# 2. Using single  quotes
-print('Hello, World!')
-
-# 3. Using a variable
-
-message = "Hello, World!"
-print(message)
-
-# 4. Using f-string
-
-name = "World"
-print(f"Hello, {name}!")
-
->>>>>>> bf75e7a44347f778b9103b7c86d921fffd7ef34e
