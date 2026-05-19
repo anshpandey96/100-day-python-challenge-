bf75e7a44347f778b9103b7c86d921fffd7ef34e
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-19 19:34:51 +0530

    Add files via upload

---
+++ Hello_world.py
@@ -0,0 +1,18 @@
+print( "Hello world!")
+
+ # 1 Basic print
+print("Hello, World!")
+
+# 2. Using single  quotes
+print('Hello, World!')
+
+# 3. Using a variable
+
+message = "Hello, World!"
+print(message)
+
+# 4. Using f-string
+
+name = "World"
+print(f"Hello, {name}!")
+
