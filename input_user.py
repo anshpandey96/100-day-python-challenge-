56308a0c0582294f528ef34786f037c21eb41fd1
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-21 00:10:11 +0530

    taking input from the user

---
+++ input_user.py
@@ -0,0 +1,14 @@
+# basic_program.py
+
+# Taking input from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Processing and output
+print("Hello", name + "!")
+print("You are", age, "years old.")
+
+# A small calculation
+next_age = age + 1
+print("Next year you will be", next_age)
+
