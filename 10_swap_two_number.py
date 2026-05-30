cf69a2748fce8c840d2eb9ef491613f246a12cbe
Ansh pandey <akashakashpandey612@gmail.com>
2026-05-30 20:59:40 +0530

    Swapping two numbers using three variables

---
+++ 10_swap_two_number.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Swapping the numbers
+temp = num1 
+num1 = num2
+num2 = temp
+
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+
+
+
