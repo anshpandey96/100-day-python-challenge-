53cd877be051a8a363ff7d82502b76fa279da268
Ansh pandey <akashakashpandey612@gmail.com>
2026-06-01 20:35:30 +0530

    To check number is prime or not

---
+++ 12_Even_or_odd.py
@@ -0,0 +1,14 @@
+
+
+
+
+num = int(input("Enter a number : "))
+
+if num % 2 == 0:  # condition to check if the number is even or odd 
+
+    print(num, "is an even number.")
+
+else:
+
+    print(num, "is an odd number.")
+
