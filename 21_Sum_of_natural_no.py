93e7d65a546f0bbcc3feb5b3adce23610f8c6935
Ansh pandey <akashakashpandey612@gmail.com>
2026-06-10 19:59:29 +0530

    TO Find the Sum of Natural Number

---
+++ 21_Sum_of_natural_no.py
@@ -0,0 +1,15 @@
+limit = int(input("Enter the limit: " ))
+  # Initialize the sum 
+sum = 0
+
+#use a for loop to calculate the sum of natural numbers
+
+for i in range(1 , limit+1):
+    sum += i
+
+# print the sum 
+print("The sum of natural numbers up to " , limit, "is : ", sum)
+
+
+
+
