f8b13470ef38a56d91ec9f413487f53209b35d69
Ansh pandey <akashakashpandey612@gmail.com>
2026-06-13 21:36:03 +0530

    Convert a  decimal to binary & octal and hexadecimal

---
+++ 24_Conversion.py
@@ -0,0 +1,9 @@
+dec_num =int(input("Enter a Deciamal Number : "))
+
+
+print("The decimal value of ", dec_num, "is: ")
+
+print(bin(dec_num),"in binary. ")
+print(oct(dec_num), "in octal. " )
+print(hex(dec_num), "in Hexadecimal. ")
+
